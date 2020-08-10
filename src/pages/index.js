3fd262ba3ae12a4090636dcbd39a91ca32a1dd62
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-08-10 23:46:06 +0530

    Add Link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const index = () => {
   return (
     <div>
       <h1>Hello World</h1>
+      <Link to="/blog">Blog Page</Link>
     </div>
   )
 }
